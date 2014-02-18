f55eb8ab3ce6dd0ca79b0bdb0532d1a9ed91e0f2
Robert Clausecker <fuzxxl@gmail.com>
2014-02-19 00:47:20 +0100

    Rename freefare.MifareTag to freefare.Tag

--- dev/freefare/tag.go
+++ dev/freefare/tag.go
@@ -12,7 +12,7 @@ import "unsafe"
 // This struct represents a Mifare tag of arbitrary type. You can figure out its
 // type with the Type() method. To access features of a specific type of tag,
 // wrap it into one of the other tag types.
-type MifareTag struct {
+type Tag struct {
 	tag  C.MifareTag
 	dev  *nfc.Device
 	info *C.nfc_iso14443a_info // may be nil
@@ -20,10 +20,10 @@ type MifareTag struct {
 
 // Wrap a C.MifareTag and set a finalizer to automatically free the tag once it
 // becomes unreachable.
-func wrapTag(t C.MifareTag, d *nfc.Device, i *C.nfc_iso14443a_info) MifareTag {
-	tag := MifareTag{t, d, i}
+func wrapTag(t C.MifareTag, d *nfc.Device, i *C.nfc_iso14443a_info) Tag {
+	tag := Tag{t, d, i}
 	runtime.SetFinalizer(
-		tag, func(t MifareTag) {
+		tag, func(t Tag) {
 			C.freefare_free_tag(t.tag)
 			C.free(unsafe.Pointer(t.info))
 		})
@@ -40,21 +40,21 @@ const (
 	DESFIRE
 )
 
-// Get the type of a MifareTag. The returned integer can be compared against the
+// Get the type of a Tag. The returned integer can be compared against the
 // supplied constants to figure out what kind of tag it is.
-func (t MifareTag) Type() int {
+func (t Tag) Type() int {
 	return int(C.freefare_get_tag_type(t.tag))
 }
 
-// Get the friendly name of a MifareTag. This function wraps
+// Get the friendly name of a Tag. This function wraps
 // freefare_get_tag_friendly_name().
-func (t MifareTag) String() string {
+func (t Tag) String() string {
 	cptr := C.freefare_get_tag_friendly_name(t.tag)
 	return C.GoString(cptr)
 }
 
-// Get the UID of a MifareTag. The UID is a string of hexadezimal digits.
-func (t MifareTag) UID() string {
+// Get the UID of a Tag. The UID is a string of hexadecimal digits.
+func (t Tag) UID() string {
 	cptr := C.freefare_get_tag_uid(t.tag)
 	defer C.free(unsafe.Pointer(cptr))
 	return C.GoString(cptr)
@@ -62,8 +62,8 @@ func (t MifareTag) UID() string {
 
 // Get a list of the MIFARE targets near to the provided NFC initiator. If the
 // list of tags cannot be generated, an error is returned. The Go wrapper takes
-// care of allocating and deallocating MifareTags. No precautions are needed.
-func GetTags(d *nfc.Device) ([]MifareTag, error) {
+// care of allocating and deallocating Tags. No precautions are needed.
+func GetTags(d *nfc.Device) ([]Tag, error) {
 	dd := devicePointer(d)
 	if dd == nil {
 		return nil, errors.New("device closed")
@@ -76,7 +76,7 @@ func GetTags(d *nfc.Device) ([]MifareTag, error) {
 	}
 
 	// freefare_get_tags returns a nil-terminated array of pointers.
-	var tags []MifareTag
+	var tags []Tag
 	for *tagptr != nil {
 		tags = append(tags, wrapTag(*tagptr, d, nil))
 
@@ -88,13 +88,13 @@ func GetTags(d *nfc.Device) ([]MifareTag, error) {
 	return tags, nil
 }
 
-// Automagically allocate a MifareTag given a device and target info. The Go
-// wrapper takes care of allocating and deallocating MifareTags. No precautions
+// Automagically allocate a Tag given a device and target info. The Go
+// wrapper takes care of allocating and deallocating Tags. No precautions
 // are needed. The Baud field of the info parameter is not evaluated.
-func NewTag(d *nfc.Device, info *nfc.ISO14443aTarget) (MifareTag, error) {
+func NewTag(d *nfc.Device, info *nfc.ISO14443aTarget) (Tag, error) {
 	dd := devicePointer(d)
 	if dd == nil {
-		return MifareTag{}, errors.New("device closed")
+		return Tag{}, errors.New("device closed")
 	}
 
 	// Marshall() actually returns an nfc_target, but it's first member is
@@ -105,7 +105,7 @@ func NewTag(d *nfc.Device, info *nfc.ISO14443aTarget) (MifareTag, error) {
 	defer C.free(unsafe.Pointer(ctag))
 	if ctag == nil {
 		C.free(unsafe.Pointer(ctag))
-		return MifareTag{}, errors.New("Could not create tag")
+		return Tag{}, errors.New("Could not create tag")
 	}
 
 	return wrapTag(ctag, d, cinfo), nil
