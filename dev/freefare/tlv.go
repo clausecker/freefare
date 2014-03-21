3c27ce79a76c5c82021d6251e388136cafc493b1
Robert Clausecker <fuzxxl@gmail.com>
2014-03-21 15:10:51 +0100

    Implement TLV functionality

---
+++ dev/freefare/tlv.go
@@ -0,0 +1,66 @@
+package freefare
+
+// Encode data stream into TLV. This function expects that len(istream) < 65536.
+// Iff this doesn't hold, this function returns nil. This function implements
+// the functionality of tlv_encode().
+func TLVencode(istream []byte, t byte) []byte {
+	if len(istream) >= 0xffff {
+		return nil
+	}
+
+	var res []byte
+	if len(istream) > 254 {
+		res = make([]byte, 4, len(istream)+5)
+		res[0] = t
+		res[1] = 0xff
+		res[2] = byte(len(istream) >> 8)
+		res[3] = byte(len(istream) >> 0)
+	} else {
+		res = make([]byte, 2, len(istream)+3)
+		res[0] = t
+		res[1] = byte(len(istream))
+	}
+
+	// copy should not occur, but better be safe
+	res = append(res, istream...)
+	res = append(res, 0xfe) // TLV_TERMINATOR
+
+	return res
+}
+
+// Decode TLV from data stream. This function implements the functionality of
+// tlv_decode().
+func TLVdecode(istream []byte) (ostream []byte, t byte) {
+	t = istream[0]
+	fls, fvs := TLVrecordLength(istream)
+
+	res := make([]byte, fvs)
+	copy(res, istream[1+fls:])
+
+	return res, t
+}
+
+// Get field length size (fls) and field value size (fvs) of a TLV record. This
+// function implements the functionality of tlv_record_length().
+func TLVrecordLength(istream []byte) (fls, fvs int) {
+	switch istream[0] {
+	case 0x00:
+	case 0xfe:
+	case 0x01:
+		fallthrough
+	case 0x02:
+		fallthrough
+	case 0x03:
+		fallthrough
+	default:
+		if istream[1] == 0xff {
+			fls = 3
+			fvs = int(istream[2])<<8 + int(istream[3])<<0
+		} else {
+			fls = 1
+			fvs = int(istream[1])
+		}
+	}
+
+	return
+}
